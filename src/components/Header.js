d3488671ef3e68216e334988eaf1041201ef6b00
David <david.owuor@student.moringaschool.com>
2023-02-11 00:00:53 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+
+function Header(){
+
+    return(
+        <div>
+            <h1>Handshake</h1>
+           <a href="!#">Jobs</a>
+           <a href="!#">Jobs</a>
+           <a href="!#">Jobs</a>
+        </div>
+    )
+}export default Header;
