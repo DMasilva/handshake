f71a40d95686da91269cb72f5b81ce57227250d5
David <david.owuor@student.moringaschool.com>
2023-02-17 16:54:15 +0300

    new

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,19 @@ function Header(){
 
     return(
         <div>
+        <div>
+            <div>
             <h1>Handshake</h1>
+            </div>
            <a href="!#">Jobs</a>
-           <a href="!#">Jobs</a>
-           <a href="!#">Jobs</a>
+           <a href="!#">Events</a>
+           <a href="!#">Employers</a>
         </div>
+         <div>
+        <a href="!#">Inbox</a>
+        <a href="!#">Career Center</a>
+
+     </div>
+     </div>
     )
 }export default Header;
